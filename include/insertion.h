1b2bb1e9c29a6b648d0331e99be6517025f327a8
djoker-koker <calab0k@yandex.ru>
2025-10-29 23:25:22 +0300

    Update insertion.h

--- include/insertion.h
+++ include/insertion.h
@@ -1,9 +1,9 @@
-#ifndef INSERTION_SORT_H
-#define INSERTION_SORT_H
+#ifndef INSERTION_H
+#define INSERTION_H
 
 #include <vector>
 
 template <typename T>
-void insertion_sort(std::vector<T>& arr);
+void insertion(std::vector<T>& arr);
 
-#endif // INSERTION_SORT_H
+#endif // INSERTION_H
