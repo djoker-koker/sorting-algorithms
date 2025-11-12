114979b7b9581cb13ad8fe3ef5b70a2450754d35
djoker-koker <calab0k@yandex.ru>
2025-11-12 05:40:56 +0300

    Update optimized_bubble.cpp

--- src/optimized_bubble.cpp
+++ src/optimized_bubble.cpp
@@ -1,25 +1,23 @@
-#include "sorting-algorithms/include/optimized_bubble.h"
-#include <algorithm>
+#include "../include/optimized_bubble_sort.h"
 
-template <typename T>
-void optimized_bubble(std::vector<T>& arr) {
-    size_t n = arr.size();
-    if (n <= 1) return;
-    
-    for (size_t i = 0; i < n - 1; i++) {
-        bool swapped = false;
-        
-        for (size_t j = 0; j < n - i - 1; j++) {
+template<typename T>
+void optimizedBubbleSort(T arr[], int n) {
+    bool swapped;
+    for (int i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
-                std::swap(arr[j], arr[j + 1]);
+                T temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
                 swapped = true;
             }
         }
-        
-        // Если не было обменов - массив уже отсортирован
-        if (!swapped) break;
+        if (!swapped) {
+            break;
+        }
     }
 }
 
-template void optimized_bubble<int>(std::vector<int>& arr);
-template void optimized_bubble<double>(std::vector<double>& arr);
+template void optimizedBubbleSort<int>(int arr[], int n);
+template void optimizedBubbleSort<double>(double arr[], int n);
