a3393b3f8ec7b8036a51db9c690db86df227f7f2
djoker-koker <calab0k@yandex.ru>
2025-11-12 05:41:17 +0300

    Update exchange.cpp

--- src/exchange.cpp
+++ src/exchange.cpp
@@ -5,7 +5,6 @@ void exchangeSort(T arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = i + 1; j < n; j++) {
             if (arr[i] > arr[j]) {
-                // Обмен элементов
                 T temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
