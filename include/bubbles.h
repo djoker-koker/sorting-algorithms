9700175778b1c4f66b094740cd3f852d053de641
djoker-koker <calab0k@yandex.ru>
2025-11-12 05:38:27 +0300

    Update bubbles.h

--- include/bubbles.h
+++ include/bubbles.h
@@ -1,9 +1,4 @@
-#ifndef BUBBLE_H
-#define BUBBLE_H
+#pragma once
 
-#include <vector>
-
-template <typename T>
-void bubble(std::vector<T>& arr);
-
-#endif // BUBBLE_H
+template<typename T>
+void bubbleSort(T arr[], int n);
