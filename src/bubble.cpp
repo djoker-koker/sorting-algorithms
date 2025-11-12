0f1076f399c3ac3f6411bf0343bd74f0ae2f110d
djoker-koker <calab0k@yandex.ru>
2025-11-12 05:39:56 +0300

    Update bubble.cpp

--- src/bubble.cpp
+++ src/bubble.cpp
@@ -1,19 +1,17 @@
-#include "sorting-algorithms/include/bubble.h"
-#include <algorithm>
+#include "../include/bubble_sort.h"
 
-template <typename T>
-void bubble(std::vector<T>& arr) {
-    size_t n = arr.size();
-    if (n <= 1) return;
-    
-    for (size_t i = 0; i < n - 1; i++) {
-        for (size_t j = 0; j < n - i - 1; j++) {
+template<typename T>
+void bubbleSort(T arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
-                std::swap(arr[j], arr[j + 1]);
+                T temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
 }
 
-template void bubble<int>(std::vector<int>& arr);
-template void bubble<double>(std::vector<double>& arr);
+template void bubbleSort<int>(int arr[], int n);
+template void bubbleSort<double>(double arr[], int n);
