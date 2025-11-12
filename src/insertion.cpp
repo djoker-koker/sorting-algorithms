b40b65036fa530e5b0daf95751db4ed221992457
djoker-koker <calab0k@yandex.ru>
2025-11-12 05:40:34 +0300

    Update insertion.cpp

--- src/insertion.cpp
+++ src/insertion.cpp
@@ -1,22 +1,19 @@
-#include "sorting-algorithms/include/insertion.h"
-#include <algorithm>
+#include "../include/insertion_sort.h"
 
-template <typename T>
-void insertion(std::vector<T>& arr) {
-    size_t n = arr.size();
-    if (n <= 1) return;
-    
-    for (size_t i = 1; i < n; i++) {
+template<typename T>
+void insertionSort(T arr[], int n) {
+    for (int i = 1; i < n; i++) {
         T key = arr[i];
-        size_t j = i;
+        int j = i - 1;
         
-        while (j > 0 && arr[j - 1] > key) {
-            arr[j] = arr[j - 1];
-            j--;
+        // Сдвигаем элементы большие чем key на одну позицию вперед
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
         }
-        arr[j] = key;
+        arr[j + 1] = key;
     }
 }
 
-template void insertion<int>(std::vector<int>& arr);
-template void insertion<double>(std::vector<double>& arr);
+template void insertionSort<int>(int arr[], int n);
+template void insertionSort<double>(double arr[], int n);
